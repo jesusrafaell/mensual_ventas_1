6586c70d8259e0861ed342f8d8dcd501944feaa8
Jesus R. Hernandez Vera <jhernandez@csc.tranred.com.ve>
2023-05-30 14:10:55 -0400

    mes

--- index.py
+++ index.py
@@ -20,16 +20,48 @@ with open(log_file, "w") as log:
     print('No existe deposito en la ruta', rutaArchivo)
     log.write('No existe deposito en la ruta' + rutaArchivo + '\n')
 
+    now = datetime.now()
+    mes_actual = now.month
+    anio_actual = now.year
+
+  #Diccionario de nombres de meses en español
+  meses_espanol = {
+      1: "ene",
+      2: "feb",
+      3: "mar",
+      4: "abr",
+      5: "may",
+      6: "jun",
+      7: "jul",
+      8: "ago",
+      9: "sep",
+      10: "oct",
+      11: "nov",
+      12: "dic"
+  }
+
   now = datetime.now()
-  mes_proceso = now.strftime("%b").lower()
+  mes_actual = now.month
+  anio_actual = now.year
 
-  print("actual", mes_proceso)
+  if mes_actual == 1:  # Si es enero, el mes anterior es diciembre del año anterior
+      mes_anterior = 12
+      anio_anterior = anio_actual - 1
+  else:
+      mes_anterior = mes_actual - 1
+      anio_anterior = anio_actual
 
-  mes_proceso = "mar"; #remove
+  fecha_anterior = datetime(anio_anterior, mes_anterior, 1)
+
+  # mes_proceso = fecha_anterior.strftime("%b").lower()
+
+  mes_proceso = meses_espanol[mes_anterior]
+
+  print("Mes anterior:", mes_proceso)
+
+  # mes_proceso = "mar"; #remove
 
   print("Mes: " + mes_proceso);
-  dia, afiliado, monto, pan, cr = None, None, None, None, None
-  #** dev
   # primero
   dirs_ad = File.get_dirs_of_dir_mes(rutaCredito, mes_proceso)
 
