e2e3f2e6f95d0e74b6086b4a11bcb0fdd2991795
Jesus R. Hernandez Vera <jhernandez@csc.tranred.com.ve>
2023-07-04 14:07:14 -0400

    main

--- variables.py
+++ variables.py
@@ -1,7 +1,7 @@
-# rutaCredito = "C:/Archivos_Depositos"
-# rutaDebito = r"\\10.198.71.18\ftp\FTP720"
-# rutaArchivo = "C:/Archivos_Depositos/"
+rutaCredito = "C:/Archivos_Depositos"
+rutaDebito = r"\\10.198.71.18\ftp\FTP720"
+rutaArchivo = "C:/Archivos_Depositos/"
 
-rutaCredito = "C:/archivos/Archivos_Credito"
-rutaDebito = "C:/archivos/Archivos_Debito/720"
-rutaArchivo = "C:/archivos/"
+# rutaCredito = "C:/archivos/Archivos_Credito"
+# rutaDebito = "C:/archivos/Archivos_Debito/720"
+# rutaArchivo = "C:/archivos/"
